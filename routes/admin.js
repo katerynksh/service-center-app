f160a7cd36946f0bbd991a190072d854a44b79b2
Sherepera Kateryna <katerynasherepera@gmail.com>
2026-05-14 12:24:45 +0300

    Refactor dashboard layout and styles

--- routes/admin.js
+++ routes/admin.js
@@ -4,27 +4,25 @@ import { isAdmin } from '../middleware/authMiddleware.js';
 import { getAdminDashboardView } from '../views/admin/dashboardAdmin.js';
 const router = express.Router();
 
-// Маршрути для адміна
-// router.get('/dashboard', isAdmin, getAdminDashboardView);
-router.get('/', getAdminDashboardView); //тестовий маршрут для відображення адмінської панелі без авторизації
-router.get('/edit/:id', admin.getEditPage);              // Сторінка редагування
-router.get('/create-order', (req, res) => {
+router.get('/', isAdmin, getAdminDashboardView);
+// router.get('/', getAdminDashboardView); //тестовий маршрут для відображення адмінської панелі без авторизації
+router.get('/edit/:id', isAdmin, admin.getEditPage);             
+router.get('/create-order', isAdmin, (req, res) => {
     res.render('admin/createOrderAdmin', { 
         title: 'Create New Order' 
     });
 });
-router.put('/orders/:id', admin.updateOrderFull);        // API для оновлення
-router.delete('/orders/:id', admin.deleteOrder);         // API для видалення
-router.post('/orders', admin.createOrderAdmin);       // API для створення нового замовлення адміном
+router.put('/orders/:id', admin.updateOrderFull);        
+router.delete('/orders/:id', admin.deleteOrder);         
+router.post('/orders', admin.createOrderAdmin);       
 
 
-// router.get('/all-info', isAdmin, admin.getAllData);
-router.get('/all-info', admin.getAllData); //тестовий маршрут для отримання всіх даних без авторизації
-router.get('/create-master', (req, res) => {
+router.get('/all-info', isAdmin, admin.getAllData);
+// router.get('/all-info', admin.getAllData); //тестовий маршрут для отримання всіх даних без авторизації
+router.get('/create-master', isAdmin, (req, res) => {
     res.render('admin/createMasterAdmin');
 });
-router.post('/masters', admin.createMaster);
-router.put('/assign-master', admin.assignMaster);
-// router.post('/assign-master', isAdmin, admin.assignMaster);
+router.post('/masters', isAdmin, admin.createMaster);
+router.put('/assign-master', isAdmin, admin.assignMaster);
 
 export default router;
--- routes/master.js
+++ routes/master.js
@@ -11,23 +11,22 @@ router.get('/', getMasterDashboardView, (req, res) => {
 res.render('master/dashboardMaster')
 });
 
-// router.get('/edit/:id', isMaster, getEditMasterView); {
-  // res.render('master/editMaster', {
-  //     title: 'Edit Order',}
-router.get('/edit/:id', getEditMasterView, (req, res) => { //тестoвий маршрут для отримання сторінки редагування замовлення майстром без авторизації// 
-  res.render('master/editMaster')
+router.get('/edit/:id', isMaster, getEditMasterView, (req, res) => {
+  res.render('master/editMaster', {
+      title: 'Edit Order'
+  });
 });
 
-// router.get('/order-details/:id', isMaster, master.getOrderDetails);
-router.get('/order-details/:id', master.getOrderDetails); //тестoвий маршрут для отримання деталей замовлення майстром без авторизації
+router.get('/order-details/:id', isMaster, master.getOrderDetails);
+// router.get('/order-details/:id', master.getOrderDetails); //тестoвий маршрут для отримання деталей замовлення майстром без авторизації
 
-// router.get('/dashboard', isMaster, master.getDashboard); 
-router.get('/dashboard', master.getDashboard); //тестовий маршрут для отримання даних майстра без авторизації
+router.get('/dashboard', isMaster, master.getDashboard); 
+// router.get('/dashboard', master.getDashboard); //тестовий маршрут для отримання даних майстра без авторизації
 
-router.put('/order/:id', master.updateOrder); //тестовий маршрут для оновлення замовлення майстром без авторизації
-// router.put('/order/:id', isMaster, master.updateOrder);
+// router.put('/order/:id', master.updateOrder); //тестовий маршрут для оновлення замовлення майстром без авторизації
+router.put('/order/:id', isMaster, master.updateOrder);
 
-router.put('/order/accept/:id', master.acceptOrder); //тестовий маршрут для прийняття замовлення майстром без авторизації
-// router.put('/order/accept/:id', isMaster, master.acceptOrder);
+// router.put('/order/accept/:id', master.acceptOrder); //тестовий маршрут для прийняття замовлення майстром без авторизації
+router.put('/order/accept/:id', isMaster, master.acceptOrder);
 
 export default router;
